be5dd09e27fb85838cb4d957d2dc8fbf03a1c579
Miguel <up202108822@up.pt>
2025-09-23 17:02:45 +0100

    added python file

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy
+import pandas
+
+awards_players = pandas.read_csv("basketballPlayoffs/awards_players.csv")
+coaches = pandas.read_csv("basketballPlayoffs/coaches.csv")
+players_teams = pandas.read_csv("basketballPlayoffs/players_teams.csv")
+players = pandas.read_csv("basketballPlayoffs/players.csv")
+series_post = pandas.read_csv("basketballPlayoffs/series_post.csv")
+teams_post = pandas.read_csv("basketballPlayoffs/teams_post.csv")
+teams = pandas.read_csv("basketballPlayoffs/teams.csv")
